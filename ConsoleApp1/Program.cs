db7e55a0c6f93a5ea9537f06f5b09e3d854ce283
699evkp <tu699evkp@code.edu.az>
2022-04-24 12:00:39 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+
+namespace taskextension
+{
+   static class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "exTensiON claSSi staTIc OLMalIdiR";
+            Console.WriteLine( text.Capitalize());
+        }
+        static StringBuilder Capitalize(this string text)
+        {
+            string newtext = " ";
+
+            StringBuilder result = new StringBuilder();
+            foreach (var item in text)
+            {
+                newtext+= char.ToLower(item);
+            }
+            result.Append(char.ToUpper(newtext[0]));
+                for (int i = 0; i < text.Length; i++)
+            {
+                if (newtext[i]==' ')
+                {
+                    result.Append(newtext[i]);
+                    result.Append(char.ToUpper(newtext[ i +1]));
+                    i++;
+                }
+                else
+                {
+                    result.Append(newtext[i]);
+                }
+            }
+            text = result.ToString();
+            return result;
+        }
+
+        
+
+        }
+
+
+    }
+
